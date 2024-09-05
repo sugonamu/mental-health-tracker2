9680def0c3e67e91d0e123634f716421a4df6c89
will <willm070905@gmail.com>
2024-09-06 00:01:19 +0700

    tutorial 1

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+def show_main(request):
+    context = {
+        'npm' : '2306123456',
+        'name': 'Pak Bepe',
+        'class': 'PBP E'
+    }
+
+    return render(request, "main.html", context)
