5adcc089533aa3a1ebab6cac449f7d207a3753aa
sugonamu <je0jwff@gmail.com>
2024-09-11 11:29:47 +0700

    Tutorial 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://pbp.cs.ui.ac.id/william.samuel/mentalhealthtracker", "https://pbp.cs.ui.ac.id/william.samuel/mentalhealthtracker"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/william.samuel/mentalhealthtracker", "https://pbp.cs.ui.ac.id/william.samuel/mentalhealthtracker"]
 
