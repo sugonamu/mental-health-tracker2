d90abae3500f5c5bd950adc67ab3c9e050c11e0d
sugonamu <je0jwff@gmail.com>
2024-09-18 10:41:01 +0700

    Tutorial 3 (Login,Logout)

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from main.views import show_main,create_mood_entry,show_xml,show_xml_by_id
+from main.views import show_main,create_mood_entry,show_xml,show_xml_by_id,register,login_user,logout_user
+
 
 app_name = 'main'
 
@@ -7,5 +8,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('xml/', show_xml, name='show_xml'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('create-mood-entry',create_mood_entry,name='create_mood_entry')
+    path('create-mood-entry',create_mood_entry,name='create_mood_entry'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout')
 ]
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q$w4=lyk*co0bhq-puk6*lw9zv927za^+ef-hjhk(+&7w46mvr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","william-samuel-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
