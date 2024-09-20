245841c7681761ef4e799b9ecfbc0f606caf4488
sugonamu <je0jwff@gmail.com>
2024-09-21 01:06:56 +0700

    q9 q10

--- functional_tests.py
+++ functional_tests.py
@@ -5,19 +5,18 @@ from selenium.webdriver.remote.webelement import WebElement
 
 class ExampleFunctionalTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver. Chrome()
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_handling_text_is_correct(self):
+    def test_heading_text_is_correct(self):
         self.browser.get("http://localhost:8000")
         element: WebElement = self.browser.find_element(by=By.TAG_NAME, value="h1")
+
         self.assertEqual("Mental Health Tracker", element.text)
 
     def test_page_title_is_correct(self):
         self.browser.get("http://localhost:8000")
-        self.assertEqual("PBD Mental Health Tracker", self.browser.title)
 
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual("PBD Mental Health Tracker", self.browser.title)
--- main/tests.py
+++ main/tests.py
@@ -8,14 +8,12 @@ class MainTest(TestCase):
         self.user = User.objects.create_user(username='tester', password='11111')
 
     def test_main_url_exists(self):
-        # Log in the user and set the cookie
         self.client.login(username='tester', password='11111')
-        self.client.cookies['last_login'] = '2024-09-20T12:00:00Z'  # Example timestamp
+        self.client.cookies['last_login'] = '2024-09-20T12:00:00Z' 
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
 
     def test_main_using_main_template(self):
-        # Log in the user and set the cookie
         self.client.login(username='tester', password='11111')
         self.client.cookies['last_login'] = '2024-09-20T12:00:00Z'
         response = self.client.get('')
@@ -28,7 +26,7 @@ class MainTest(TestCase):
     def test_strong_mood_user(self):
         now = timezone.now()
         mood = MoodEntry.objects.create(
-            user=self.user,  # Ensure mood entry is associated with the test user
+            user=self.user,
             mood="Happy",
             time=now,
             feelings="I'm happy, even though my clothes are soaked from the rain :(",
@@ -37,9 +35,8 @@ class MainTest(TestCase):
         self.assertTrue(mood.is_mood_strong)
 
     def test_main_template_uses_correct_page_title(self):
-        # Log in the user and set the cookie
         self.client.login(username='tester', password='11111')
         self.client.cookies['last_login'] = '2024-09-20T12:00:00Z'
         response = self.client.get("/")
         html_response = response.content.decode("utf8")
-        self.assertIn("My Application", html_response)
+        self.assertIn("PBD Mental Health Tracker", html_response)
